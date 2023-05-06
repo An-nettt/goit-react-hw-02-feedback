69d37d48f02be5788b3d8393a091c599ae7abc32
Anna Kravets <annakravets1906@gmail.com>
2023-05-06 11:32:20 +0300

    Add feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+export default class App extends Component {
+  state = {
+    good: 5,
+    neutral: 0,
+    bad: 0,
+  };
+  // options;
+
+  handlerClickGood = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
+
+  handlerClickNeutral = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+
+  handlerClickBad = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () =>
+    this.setState(prevState => {
+      return { total: prevState.good };
+    });
+
+  // countPositiveFeedbackPercentage()
+
+  render() {
+    return (
+      <div>
+        <Section title="Please leave feedback" />
+        <FeedbackOptions
+          onClickGood={this.handlerClickGood}
+          onClickNeutral={this.handlerClickNeutral}
+          onClickBad={this.handlerClickBad}
+        />
+
+        <Section title="Statistics" />
+        <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          total={this.countTotalFeedback}
+        />
+      </div>
+    );
+  }
+}
